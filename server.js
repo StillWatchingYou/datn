88262f36f309a952e04afa01b14e92894d1d1b6b
StillWatchingYou <bumchatbumchat@gmail.com>
2024-01-16 02:43:56 +0700

    sobo2

--- server.js
+++ server.js
@@ -18,90 +18,118 @@ let dataStore = [];
 
 // Connect to MongoDB Atlas when the application starts
 client.connect()
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
 
 // Handle SIGINT (Ctrl+C) to close MongoDB connection gracefully
 process.on('SIGINT', async () => {
-    try {
-        await client.close();
-        console.log('MongoDB connection closed.');
-        process.exit();
-    } catch (error) {
-        console.error('Error closing MongoDB connection:', error);
-        process.exit(1);
-    }
+  try {
+    await client.close();
+    console.log('MongoDB connection closed.');
+    process.exit();
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error);
+    process.exit(1);
+  }
 });
 
-app.get('/api/data/fetch', async (req, res) => {
-    try {
-        const database = client.db('node_test');
-        const collection = database.collection('test');
-        const data =  await collection.find({}).sort({ _id: -1 }).limit(5).toArray();
-        res.json(data);
-        data.reverse();
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get('/api/data/fetchch4', async (req, res) => {
+  try {
+    const database = client.db('test2');
+    const collection = database.collection('test-ch4');
+    const data = await collection.find({}).sort({ _id: -1 }).limit(5).toArray();
+    res.json(data);
+    data.reverse();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/api/data/fetchlpg', async (req, res) => {
+  try {
+    const database = client.db('test2');
+    const collection = database.collection('test-lpg');
+    const data = await collection.find({}).sort({ _id: -1 }).limit(5).toArray();
+    res.json(data);
+    data.reverse();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/api/data', async (req, res) => {
-    try {
-        const { temperature, humidity, timestamp } = req.body;
-        console.log(`Received data - Temperature: ${temperature}, Humidity: ${humidity}, Timestamp: ${timestamp}`);
+  try {
+    const { temperature, humidity, timestamp } = req.body;
+    console.log(`Received data - Temperature: ${temperature}, Humidity: ${humidity}, Timestamp: ${timestamp}`);
 
-        const newData = {
-            Temp: temperature,
-            Humi: humidity,
-            Timestamp: timestamp,
-        };
-
-        await pushdata(newData);
-        res.status(200).send('Data saved successfully');
-    } catch (error) {
-        console.error('Error handling data:', error);
-        res.status(500).send('Internal Server Error');
+    const newData = {
+      Temp: temperature,
+      Humi: humidity,
+      Timestamp: timestamp,
+    };
+    if (newData.Humi == 0) {
+      await pushdatatoch4(newData);
+    } else {
+      await pushdatatolpg(newData);
     }
+    res.status(200).send('Data saved successfully');
+  } catch (error) {
+    console.error('Error handling data:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-async function pushdata(newData) {
-    try {
-        const database = client.db('node_test');
-        const collection = database.collection('test');
-        await collection.insertOne(newData);
-        console.log('Data inserted:', newData);
-    } catch (error) {
-        console.error('Error pushing data to MongoDB:', error);
-    }
+async function pushdatatoch4(newData) {
+  try {
+    const database = client.db('test2');
+    const collection = database.collection('test-ch4');
+    await collection.insertOne(newData);
+    console.log('Data inserted:', newData);
+  } catch (error) {
+    console.error('Error pushing data to MongoDB:', error);
+  }
+}
+
+async function pushdatatolpg(newData) {
+  try {
+    const database = client.db('test2');
+    const collection = database.collection('test-lpg');
+    await collection.insertOne(newData);
+    console.log('Data inserted:', newData);
+  } catch (error) {
+    console.error('Error pushing data to MongoDB:', error);
+  }
 }
 
 app.post('/api/control', (req, res) => {
-    try {
-        const { value } = req.body;
-        value1.shift();
-        value1.push({ value });
-        console.log('Received control value:', value);
-        res.status(200).send('Control signal received');
-    } catch (error) {
-        console.error('Error handling control signal:', error);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const { value } = req.body;
+    value1.shift();
+    value1.push({ value });
+    console.log('Received control value:', value);
+    res.status(200).send('Control signal received');
+  } catch (error) {
+    console.error('Error handling control signal:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/api/control', (req, res) => {
-    res.json(value1);
+  res.json(value1);
 });
 
 app.get('/api/data', (req, res) => {
-    res.json(dataStore);
+  res.json(dataStore);
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
